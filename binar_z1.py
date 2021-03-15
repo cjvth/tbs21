c19b2b714ef81849cdc914dc2f4a3aee32488fa3
Timofey X <vuki03@mail.ru>
2021-03-15 22:59:44 +0300

    final z1

--- binar_z1.py
+++ binar_z1.py
@@ -3,14 +3,19 @@ import pandas as pd
 from hamming_list import hamming_decode, hamming_encode
 
 cycles = {'PD1': (1950, 3150),
-          'PD2': (1030, 2010),
+          'PD2': (1200, 2300),
           'PD3': (840, 1600)}
 
-DRAW = False
+DRAW = True
 
 f = open('data/z1.dat', 'r')
 
 df = pd.read_csv('data/z1.dat', sep=' ', index_col='Tick')
+
+
+def ll(arr):
+    return ''.join(map(str, arr))
+
 
 for i in cycles:
     ch = []
@@ -26,18 +31,22 @@ for i in cycles:
             prev = j
             cur = 1 - cur
     ch = ch[1:]
-    print(min(filter(lambda x: x[2] == 0, ch), key=lambda x: x[1])[1], min(filter(lambda x: x[2] == 1, ch), key=lambda x: x[1])[1])
+    # print(min(filter(lambda x: x[2] == 0, ch), key=lambda x: x[1])[1], min(filter(lambda x: x[2] == 1, ch), key=lambda x: x[1])[1])
     ml = (min(filter(lambda x: x[2] == 0, ch), key=lambda x: x[1])[1] + min(filter(lambda x: x[2] == 1, ch), key=lambda x: x[1])[1]) // 2
+    # print(360 * ml / sum([j[1] for j in ch]))
     er = []
     li = []
     for j in ch:
         li += [j[2]] * round(j[1] / ml)
         er.append((j[2], j[1] / ml))
-    print(li)
-    print(er)
-    print(hamming_decode(li, True))
-    print(hamming_decode(li[::-1], True))
-    print()
+    print('not reversed')
+    dec = hamming_decode(li, True)
+    print(ll(dec[0]), dec[1])
+    print(ll(hamming_encode(hamming_decode(li))))
+    print('reversed')
+    dec = hamming_decode(li[::-1], True)
+    print(ll(dec[0]), dec[1])
+    print(ll(hamming_encode(hamming_decode(li[::-1]))))
 
 if DRAW:
     for i in cycles:
