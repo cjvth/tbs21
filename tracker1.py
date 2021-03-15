85aab4268defc60e12ad97408f396b54d5aadb3b
KakahuSiel <68685567+KakahuSiel@users.noreply.github.com>
2021-03-15 19:42:49 +0700

    Update tracker1.py

--- tracker1.py
+++ tracker1.py
@@ -17,10 +17,10 @@ class tracker:
         dx=dx-4096
 #            tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
    speed=arduino_map(abs(int(dx)),0,2047,0,10)#int(arduino_map(abs(int(dx)),0,2047,0,40))
-   speed*=10
+   speed*=5
    speed=int(speed)
    if True:#(#abs(int(dx))<500):
-        if (abs(int(dx))<10):
+        if (abs(int(dx))<15):
              c2s.moveStop()
         else:
              if(int(dx)>0):
