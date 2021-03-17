bb797cf732b526cfb8323a059f5253802bc53939
KakahuSiel <68685567+KakahuSiel@users.noreply.github.com>
2021-03-17 14:16:49 +0700

    Update tracker0.py

--- tracker0.py
+++ tracker0.py
@@ -9,7 +9,7 @@ class tracker:
    dx=int(status)&0x0fff
    if(dx>2048):
     dx=dx-4096
-    tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
+   tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
    if(abs(int(dx))<500):
     if(abs(int(dx))<10):
      c2s.moveStop()
