48cbe2ecc4c57f043b0fb21232606927b561fd3d
KakahuSiel <68685567+KakahuSiel@users.noreply.github.com>
2021-03-17 15:21:06 +0700

    Create FUCKmyASSholeBITCHES.py

---
+++ FUCKmyASSholeBITCHES.py
@@ -0,0 +1,23 @@
+from client2server import client2server
+
+class tracker:
+ def run(tracklog):
+  c2s=client2server()
+  i=0
+  while i==0:
+   status=c2s.getStatus()
+   dx=int(status)&0x0fff
+   if(dx>2048):
+    dx=dx-4096
+
+   tracklog.write("Received dx = {},  speed ={}  \n".format(0,1).encode())#tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
+   if(abs(int(dx))<500):
+    if(abs(int(dx))<10):
+     c2s.moveStop()
+    else:
+     if(int(dx)>0):
+      c2s.moveLeft(5)
+     else:
+      c2s.moveRight(5)
+#            return
+  c2s.__finit__
