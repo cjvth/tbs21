2d543b1767a7ff30b8379d134f11148b5f911861
KakahuSiel <68685567+KakahuSiel@users.noreply.github.com>
2021-03-17 15:30:04 +0700

    Update FUCKmyASSholeBITCHES.py

--- FUCKmyASSholeBITCHES.py
+++ FUCKmyASSholeBITCHES.py
@@ -9,8 +9,27 @@ class tracker:
    dx=int(status)&0x0fff
    if(dx>2048):
     dx=dx-4096
-
+      
    tracklog.write("Received dx = {},  speed ={}  \n".format(0,1).encode())#tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
+   max_speed=55
+   K_p=10
+   K_d=0
+   K_i=0
+   I=0
+   last_dx=0
+   delta_t=0.1
+   f=1   
+
+
+
+
+
+
+
+
+
+
+
    if(abs(int(dx))<500):
     if(abs(int(dx))<10):
      c2s.moveStop()
