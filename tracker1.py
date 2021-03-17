6453574cefb759f34eb9fc260e1be13d213f1156
KakahuSiel <68685567+KakahuSiel@users.noreply.github.com>
2021-03-17 19:43:54 +0700

    1
    1

--- tracker1.py
+++ tracker1.py
@@ -24,25 +24,3 @@ class tracker:
    
    dx=int(status)&0x0fff
    tracklog.write("dx = {}\n".format(dx).encode()) 
-
-   #speed=int(arduino_map(abs(int(dx)),0,4096,0,100))
-   if(dx>2048):
-        dx=dx-4096
-#            tracklog.write("Received %d %d %d %d %d\n" % (dx,state,position,ticks,key0))
-   speed=arduino_map(abs(int(dx)),0,2048,0,100)#int(arduino_map(abs(int(dx)),0,2047,0,40))
-   speed*=30#15
-   speed=round(speed)
-   if speed>55:
-       speed=55
-   #speed=80
-   
-   if True:#(#abs(int(dx))<500):
-        if (abs(int(dx))<10):
-             c2s.moveStop()
-        else:
-             if(int(dx)>0):
-                  c2s.moveLeft(speed)
-             else:
-                  c2s.moveRight(speed)
-#               return
-  c2s.__finit__
